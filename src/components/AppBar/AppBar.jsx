f9c0502e837102305fae702f460665f895eb95b1
Budankov Kostya <107200706+Budankov@users.noreply.github.com>
2023-06-02 02:47:15 +0300

    Update

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,12 +1,44 @@
-import { Link } from 'react-router-dom';
-
-import './AppBar.module.scss';
+import { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { Spin as Hamburger } from 'hamburger-react';
+import styles from './AppBar.module.scss';
 
 const AppBar = () => {
+  const location = useLocation();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleToggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false); // Закриваємо бургер-меню
+  };
+
+  useEffect(() => {
+    closeMenu(); // Закриваємо бургер-меню при зміні шляху маршруту
+  }, [location.pathname]);
+
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/tweets">Tweets</Link>
+    <nav className={styles.nav__container}>
+      <div className={styles.menu__icon}>
+        <Hamburger toggled={menuOpen} toggle={handleToggleMenu} />
+      </div>
+      {menuOpen && (
+        <div className={styles.overlay} onClick={handleToggleMenu} />
+      )}
+      <ul className={`${styles.nav__list} ${menuOpen ? styles.open : ''}`}>
+        <li className={styles.nav__item}>
+          <Link className={styles.nav__link} to="/" onClick={closeMenu}>
+            Home
+          </Link>
+        </li>
+        <li className={styles.nav__item}>
+          <Link className={styles.nav__link} to="/tweets" onClick={closeMenu}>
+            Tweets
+          </Link>
+        </li>
+      </ul>
     </nav>
   );
 };
